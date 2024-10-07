83de5c8d8ce816b00a31a2df39d56b51709d161c
Stefan Babatie <stefan.babatie@nxp.com>
2024-10-07 13:52:31 +0300

    [MCZB-2440] NCP_HOST: examples: zb_common: implement clear security material set SerialLink command
    Signed-off-by: Stefan Babatie <stefan.babatie@nxp.com>

--- examples/zb_common/serial_link_wkr.h
+++ examples/zb_common/serial_link_wkr.h
@@ -531,7 +531,8 @@ typedef enum
     E_SL_MSG_FIND_BIND_ENTRY_FOR_CLUSTER_ID         =   0x909AU,
     E_SL_MSG_TC_STATUS                              =   0x909BU,
     E_SL_MSG_ZDO_BIND_EVENT						    =   0x909CU,
-    E_SL_MSG_NWK_NIB_CLEAR_TABLES                   =   0x909DU
+    E_SL_MSG_NWK_NIB_CLEAR_TABLES                   =   0x909DU,
+    E_SL_MSG_NWK_CLEAR_MAT_SET                      =   0x909EU
 
 } teSL_MsgType;
 typedef enum
