34f1339d229d304181e4d023d367edf9c5dda733
Stefan Babatie <stefan.babatie@nxp.com>
2024-11-01 11:43:14 +0200

    [MCZB-2489] NCP_HOST: platform: set mbedtls origin as GIT for NCP host
    Signed-off-by: Stefan Babatie <stefan.babatie@nxp.com>

--- examples/zigbee_coordinator/build_linux/app_config.cmake
+++ examples/zigbee_coordinator/build_linux/app_config.cmake
@@ -1,7 +1,7 @@
 set(CONFIG_ZB_TRACE_APP ON CACHE BOOL "Enable TRACE_APP logs")
 set(CONFIG_ZB_TRACE_ZCL ON CACHE BOOL "Enable TRACE_ZCL logs")
 set(CONFIG_ZB_DEBUG_APP ON CACHE BOOL "Enable App debug information")
-set(CONFIG_MBEDTLS_SOURCE "SDK" CACHE STRING "Use mbedtls from SDK")
+set(CONFIG_MBEDTLS_SOURCE "GIT" CACHE STRING "Use mbedtls from GIT")
 set(CONFIG_MBEDTLS_VERSION "2.28" CACHE STRING "Minimum required Mbedtls version")
 set(CONFIG_ZB_TARGET_32B OFF CACHE BOOL "Disable 32 bit support")
 set(CONFIG_ZB_OTA_SUPPORT ON CACHE BOOL "Enable OTA support")
