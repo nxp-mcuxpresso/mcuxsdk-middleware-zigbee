3313c2dc2e645709c78cbea73b699a6b1c52e68d
Anca Jeanina Dragusin <ancajeanina.dragusin@nxp.com>
2024-11-08 13:13:26 +0200

    [MCZB-2517] NCP_HOST: examples: coordinator & router
    For imx8 yocto recipe it is easier to provide mbedtls source
    as command line parameter rather then edit app_config.cmake file.
    
    Signed-off-by: Anca Jeanina Dragusin <ancajeanina.dragusin@nxp.com>

--- examples/zigbee_router/build_linux/app_config.cmake
+++ examples/zigbee_router/build_linux/app_config.cmake
@@ -5,7 +5,6 @@
 set(CONFIG_ZB_TRACE_APP ON CACHE BOOL "Enable TRACE_APP logs")
 set(CONFIG_ZB_TRACE_ZCL ON CACHE BOOL "Enable TRACE_ZCL logs")
 set(CONFIG_ZB_DEBUG_APP ON CACHE BOOL "Enable App debug information")
-set(CONFIG_MBEDTLS_SOURCE "GIT" CACHE STRING "Use mbedtls from GIT")
 set(CONFIG_MBEDTLS_VERSION "2.28" CACHE STRING "Minimum required Mbedtls version")
 set(CONFIG_ZB_TARGET_32B OFF CACHE BOOL "Disable 32 bit support")
 set(CONFIG_ZB_OTA_SUPPORT OFF CACHE BOOL "Disable OTA support")
