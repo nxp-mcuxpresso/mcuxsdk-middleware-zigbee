8a26639165bd8c795a8ec18f63c9b47b2c641ab1
Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
2025-02-25 17:02:26 +0100

    [MCRED-837] examples: zigbee_ble: Fix build failures for commissioning apps
    EdgeFast generated header files got renamed and squached to
    a single one "edgefast_bluetooth_config".

--- examples/zigbee_ble/edgefast_common/zigbee_edgefast_bluetooth_app.h
+++ examples/zigbee_ble/edgefast_common/zigbee_edgefast_bluetooth_app.h
@@ -7,6 +7,4 @@
 /****************************************************************************/
 /***        Include files                                                 ***/
 /****************************************************************************/
-#include "edgefast_bluetooth_config_Gen.h"
-#include "edgefast_bluetooth_debug_config_Gen.h"
-#include "edgefast_bluetooth_extension_config_Gen.h"
+#include "edgefast_bluetooth_config.h"
